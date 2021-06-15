6ddb6abd6490707a1202cf8ac7f983743631146c
Hamza Ben Yazid <benyazidhamza969@gmail.com>
2021-06-15 19:06:10 +0100

    -add sound instructions -freeze camera when picture is taked

--- back-end/api/predict/models.py
+++ back-end/api/predict/models.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework import status
 import os
 
-CLASSES = ["10 dh","100 dh","20 dh","200 dh","5 dh","50 dh"]
+CLASSES = ["10 dirhams","100 dirhams","20 dirhams","200 dirhams","5 dirhams","50 dirhams"]
 model_name = '/model_v1'
 model_weight_name = '/model.weights.best.hdf5'
 model = tf.keras.models.load_model("static/" + 'model' + model_name)
--- front-end/components/Camera.js
+++ front-end/components/Camera.js
@@ -6,6 +6,7 @@ import * as ImageManipulator from 'expo-image-manipulator';
 import PredBut from './predictButton';
 import Loading from './Loading';
 import { StatusBar } from 'expo-status-bar';
+import * as Speech from 'expo-speech';
 
 class Cam extends React.Component {
     styles = StyleSheet.create({
@@ -20,8 +21,9 @@ class Cam extends React.Component {
         type: Camera.Constants.Type.back,
         data : {},
         loading : false,
-        flash : Camera.Constants.FlashMode.off,
+        flash : "off",
         lastpress: new Date().getTime(),
+        firstLaunch : true,
     }
 
     ref = React.createRef();
@@ -29,14 +31,23 @@ class Cam extends React.Component {
     async componentDidMount(){
           const { status } = await Camera.requestPermissionsAsync();
           this.setState({...this.state,hasPermission : (status === 'granted')})
+          if (this.state.firstLaunch){
+            Speech.speak("Hi.")
+            Speech.speak("Welcome to currency recognition app.");
+            Speech.speak("To scan your money, long press the screen for one second.");
+            Speech.speak("To activate or deactivate the flash, double click the screen.");
+            this.setState({...this.state,firstLaunch : false});
+          }
     }
     predict = async (event) => {
       
       if (this.ref && !this.state.loading) {
         await this.ref.current.takePictureAsync({onPictureSaved: this.uploadimage})
         this.setState({...this.state,loading:true});
-          //this.ref.current.pausePreview();
-          //this.ref.current.resumePreview();
+
+        Speech.speak("Scanning... This might take few seconds.")
+        this.ref.current.pausePreview();
+        
       }
     }
     uploadimage = async (dt) => {
@@ -49,24 +60,26 @@ class Cam extends React.Component {
       console.log("uploading image ...");
       var dtform = new FormData();
       dtform.append('file', { uri: manipResult.uri, name: 'picture.jpg', type: 'image/jpg' });
-      axios.post('http://75bf612f4f98.ngrok.io/predict/predict/predict/', dtform).then(responseData => {
+      axios.post('http://397348c0cd54.ngrok.io/predict/predict/predict/', dtform).then(responseData => {
           this.setState({...this.state,loading:false})
-          alert(responseData.data.result);
+          //alert(responseData.data.result);
+          Speech.speak("This is "+responseData.data.result)
           console.log(responseData.data);
+          this.ref.current.resumePreview();
       })
       .catch(err => { console.log(err); });
     }
     activateFlash = async (event) => {
+        Speech.stop();
         var delta = new Date().getTime() - this.state.lastPress;
         if(delta < 200) {
-           this.setState({...this.state,flash :(this.state.flash===Camera.Constants.FlashMode.on)
-            ?Camera.Constants.FlashMode.off
-            :Camera.Constants.FlashMode.on
-          });
+          var next_flash_state = (this.state.flash==="on")?"off":"on";
+          Speech.speak("   flash "+next_flash_state);
+          this.setState({...this.state,flash :next_flash_state});
         }else{
           this.setState({
-              ...this.state,
-              lastPress: new Date().getTime()
+            ...this.state,
+            lastPress: new Date().getTime()
           });
         }
     }
@@ -89,7 +102,7 @@ class Cam extends React.Component {
                 style={[this.styles.camera]}
                 onLongPress={this.predict}
                 onPress={this.activateFlash}
-                delayLongPress={1500}>
+                delayLongPress={1000}>
               </TouchableOpacity>
             <View style={this.styles.buttonContainer}>
                 <PredBut press = {this.predict}/>
